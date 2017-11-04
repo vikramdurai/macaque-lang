18ca7bc396a5c979d370c19293406aa41b295ae4
Vikram Durai <vikramvdurai@gmail.com>
2017-11-04 19:51:35 +0530

    Added if statements -- you can't execute code blocks yet, but they do return true/false values. Macaque is now truthy!

--- main.go
+++ main.go
@@ -35,18 +35,27 @@ func (c *Command) addArgument(arg interface{}) {
 	Execute executes a command, by using what's
 	implied by the command name.
 */
-func Execute(c *Command) {
+func execute(c *Command) {
 	switch c.Name {
 	case "print":
 		for _, v := range c.Arguments {
 			fmt.Print(v)
 		}
 		fmt.Print("\n")
+	case "if":
+		arg := c.Arguments[0]
+		test := arg.(func() bool)
+		if ok := test(); ok == true {
+			fmt.Println("=> true")
+		} else {
+			fmt.Println("=> false")
+		}
 	}
 }
 
 func fail(reason error) {
-	fmt.Errorf("macaque: %v\n", reason)
+	err := fmt.Errorf("macaque: %v\n", reason)
+	fmt.Printf("%v", err)
 	os.Exit(1)
 }
 
@@ -54,7 +63,7 @@ func fail(reason error) {
 	Lex is a tokenizer that depends on
 	whitespace for hints
 */
-func Lex(linum int, line string) {
+func lex(linum int, line string) {
 	tokens := strings.Split(line, " ")
 	switch tokens[0] {
 	case "print":
@@ -71,17 +80,72 @@ func Lex(linum int, line string) {
 			case int:
 				argument = strconv.Itoa(v.(int))
 			default:
-				err := fmt.Sprintf("line %d: variable %s has unknown type or not declared",
+				err := fmt.Sprintf("line %v: variable %s has unknown type or not declared",
 					linum, tokens[1])
 				fail(errors.New(err))
 			}
 		}
 		cmd.addArgument(argument)
-		Execute(cmd)
+		execute(cmd)
+
 	case "var":
-		vars[tokens[1]] = strings.Join(tokens[3:], " ")
+		arg := strings.Join(tokens[3:], " ")
+		name := tokens[1]
+		// check if value is an int
+		new_arg, err := strconv.Atoi(arg)
+		if err != nil {
+			// int conversion failed, let's try bool
+			new_arg, err := strconv.ParseBool(arg)
+			if err != nil {
+				// bool conversion failed, so value is a string
+				vars[name] = arg
+			} else {
+				vars[name] = new_arg
+			}
+		} else {
+			vars[name] = new_arg
+		}
+
+	case "if":
+		if strings.HasPrefix(tokens[1], "(") {
+			if strings.HasSuffix(tokens[1], ")") {
+				cmd := &Command{Name: "if"}
+				// the test is usually about a variable
+				test := strings.TrimSuffix(strings.TrimPrefix(tokens[1], "("), ")")
+				v := vars[test]
+				switch v.(type) {
+				case int:
+					cmd.addArgument(func() bool {
+						if v.(int) >= 1 {
+							return true
+						} else {
+							return false
+						}
+					})
+				case string:
+					cmd.addArgument(func() bool {
+						if len(v.(string)) == 2 {
+							return false
+						} else {
+							return true
+						}
+					})
+				case bool:
+					cmd.addArgument(func() bool {
+						return v.(bool)
+					})
+				}
+				execute(cmd)
+			} else {
+				err := fmt.Sprintf("line %v: missing ending parenthesis", linum)
+				fail(errors.New(err))
+			}
+		} else {
+			err := fmt.Sprintf("line %v: test unparenthesized", linum)
+			fail(errors.New(err))
+		}
 	default:
-		err := fmt.Sprintf("line %d: unknown identifer %s", linum, tokens[0])
+		err := fmt.Sprintf("line %v: unknown identifer %s", linum, tokens[0])
 		fail(errors.New(err))
 	}
 }
@@ -98,6 +162,6 @@ func main() {
 	program := strings.Split(string(file), "\n")
 	// iterate over the lines in the program
 	for linenum, line := range program {
-		Lex(linenum, line)
+		lex(linenum, line)
 	}
 }
